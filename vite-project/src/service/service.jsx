4e2cd72d996ccc0e49b6b888507e67b577e92c82
cortez <fabien.cortez@stilog.com>
2024-07-22 11:40:49 +0200

    .

--- vite-project/src/service/service.jsx
+++ vite-project/src/service/service.jsx
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie';
-let url = process.env.BACKEND_URL || 'http://localhost:8000';
+let url = import.meta.env.BACKEND_URL || 'http://localhost:8000';
 
 const getCookie = () =>{
   const cookie = Cookies.get('userCookie');
